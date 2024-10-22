969ad60cde7d3cfa3c3d3f825d8ef29b84294374
git-for-work <akogachechiladze22@gmail.com>
2024-10-23 03:09:18 +0400

    added crud and bulk base repositories

---
+++ Resorter.Domain/Repositories/IBulkRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Resorter.Domain.Repositories;
+
+public interface IBulkRepository<T> where T : class
+{
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task AddAllAsync(IEnumerable<T> objs);
+    Task DeleteAllAsync(IEnumerable<T> objs);
+    Task SaveChanges();
+}
