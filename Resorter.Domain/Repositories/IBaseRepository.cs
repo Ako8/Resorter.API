1ff82a466f5d4ac0c058095b21d8fef235bfdf94
git-for-work <akogachechiladze22@gmail.com>
2024-10-23 02:11:31 +0400

    Added base repo

---
+++ Resorter.Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Resorter.Domain.Repositories;
+
+public interface IBaseRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task SaveChanges();
+}
